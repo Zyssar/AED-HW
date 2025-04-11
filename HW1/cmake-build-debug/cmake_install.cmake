97bc898d8a859249df5f5f05f91c2224524ea395
Zyssar <cesar.cabrera.pe@gmail.com>
2025-04-10 22:22:07 -0500

    arreglar + agregar

--- HW1/cmake-build-debug/cmake_install.cmake
+++ HW1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Cesar/CLionProjects/ForwardList
+# Install script for directory: C:/Users/Cesar/Desktop/AED-HW/HW1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Cesar/CLionProjects/ForwardList/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Cesar/Desktop/AED-HW/HW1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //esto para debuggear :p
+#include <iostream>
 
 using namespace std;
 
@@ -18,17 +18,23 @@ public:
     }
 
     T front() {
-        if (head==nullptr){throw out_of_range();}
-        return head->data;
+        if (head !=nullptr) {
+            return head -> data;;
+        }
+        return T();
     }
 
     T back() {
         node<T>* temp = head;
-        if (head==nullptr){throw out_of_range();}
-        while (temp->next!=nullptr) {
-            temp = temp->next;
+        if (head !=nullptr) {
+            node<T>* temp = head;
+            while (temp -> next !=nullptr) {
+                temp = temp -> next;
+            }
+            return temp -> data;
         }
-        return temp->data;
+
+        return T();
     }
 
     void push_front(T data) {
@@ -38,62 +44,59 @@ public:
         head = temp;
     }
 
-    void push_back(T data) {
-
-        auto* newNode = new node<T>;
+    void push_back(T data){
+        node<T>* newNode = new node<T>();
         newNode->data = data;
         newNode->next = nullptr;
-        if (head==nullptr) {
+        if(!head){
             head = newNode;
-            return head;
-        }
-        else{
+        }else{
             node<T>* temp = head;
-            while (temp->next != nullptr) {
+            while(temp->next != nullptr){
                 temp = temp->next;
             }
+            temp->next = newNode;
         }
-        temp-> next = newNode;
     }
 
-    T pop_front() {
-        if (head==nullptr){throw out_of_range();}
-        node<T>* temp = head;
-        head = head->next;
-        T data = temp->data;
-        delete temp;
-        return data;
-    }
-
-    T pop_back() {
-        if(head==nullptr){throw out_of_range();}
-        if (head->next == nullptr) {
+    T pop_front(){
+        if(head){
+            node<T>* temp = head;
+            head = head->next;
             T data = head->data;
-            delete head;
-            head = nullptr;
+            delete temp;
             return data;
         }
-        node<T>* temp = head;
-        node<T>* last = head;
-        while (temp->next->next!=nullptr) {
-            temp=temp->next;
-            cout << endl << "working" << endl;
-        }
-        last = temp->next;
-        T data = last->data;
+        return T();
+    }
 
-        delete last;
-        temp->next = nullptr;
-        return data;
+    T pop_back(){
+        if(head){
+            node<T>* temp = head;
+            node<T>* prev = nullptr;
+            while(temp->next != nullptr){
+                prev = temp;
+                temp = temp->next;
+            }
+            if(prev){
+                prev->next = nullptr;
+            }else{
+                head = nullptr;
+            }
+            T data = temp->data;
+            delete temp;
+            return data;
+        }
+        return T();
     }
 
     T operator[](const int index) {
+        if (head==nullptr) {
+            throw out_of_range("empty list");
+        }
         node<T>* temp = head;
         for (int i = 0; i < index && temp != nullptr; i++) {
             temp = temp->next;
-        }
-        if (temp==nullptr) {
-            return NULL;
         }
         return temp->data;
     }
@@ -180,9 +183,134 @@ public:
         head = temp.head;
     }
 
+    void insert(int index, T data){
+        if(index == 0){
+            push_front(data);
+        }else{
+            node<T>* temp = head;
+            for(int i = 0; i < index - 1; i++){
+                if(temp->next == nullptr){
+                    return;
+                }
+                temp = temp->next;
+            }
+            node<T>* newNode = new node<T>();
+            newNode->data = data;
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void remove(int index){
+        if(head != nullptr){
+            if(index == 0){
+                pop_front();
+            }else{
+                node<T>* temp = head;
+                for(int i = 0; i < index - 1; i++){
+                    if(temp->next == nullptr){
+                        return;
+                    }
+                    temp = temp->next;
+                }
+                if(temp->next != nullptr){
+                    node<T>* toDelete = temp->next;
+                    temp->next = toDelete->next;
+                    delete toDelete;
+                }
+            }
+        }
+    }
+    template <typename U>
+    friend ostream& operator<<(ostream& os, const forwardList<U>& list);
 };
+
+template <typename U>
+ostream& operator<<(ostream& os, const forwardList<U>& list) {
+    node<U>* temp = list.head;
+    while (temp != nullptr) {
+        os << temp->data;
+        if (temp->next != nullptr) os << " -> ";
+        temp = temp->next;
+    }
+    return os;
+}
 
 
 int main() {
+    cout << "TESTS LISTAS VACIAS:" << endl;
+    forwardList<double> list1;
+    cout << list1.front() << endl;
+    cout << list1.back() << endl;
+    cout << list1.pop_front() << endl;
+    cout << list1.pop_back() << endl;
+    cout << list1.size() << endl;
+
+
+    // test push
+    cout << "\n";
+    cout << "TEST PUSH:" << endl;
+    forwardList<string> list2;
+    list2.push_front("front1");
+    list2.push_front("front2");
+    list2.push_back("back1");
+    list2.push_back("back2");
+    cout << list2.front() << endl;
+    cout << list2.back() << endl;
+
+
+    // test []
+    cout << "\n";
+    cout << "TEST []:" << endl;
+    cout << "Imprimiendo " <<list2.size() << " elementos:"<< endl;
+    for (int i = 0; i < list2.size(); i++){
+        cout <<"- "<< list2[i] << endl;
+    }
+
+
+    // test pop
+    cout << "\n";
+    cout << "TEST POP:" << endl;
+    cout << list2.pop_front() << endl;
+    cout << list2.pop_back() << endl;
+    cout << list2.front() << endl;
+
+
+    // test clear
+    cout << "\n";
+    cout << "TEST CLEAR:" << endl;
+    cout << "Lista incial: "<<list2<< endl;
+    list2.clear();
+    cout << "Lista final: "<<list2.size() << endl;
+
+
+    // test reverse, insert, remove, sort
+    cout << "\n";
+    cout << "TEST RESERVE, INSERT, REMOVE, SHORT:" << endl;
+    forwardList<int> list3;
+    list3.push_front(5);
+    list3.push_front(2);
+    list3.push_front(4);
+    list3.push_front(8);
+    list3.insert(99,7);
+    list3.remove(7);
+    list3.push_front(1);
+    list3.insert(5,3);
+    list3.push_front(9);
+    list3.push_front(6);
+    list3.insert(6, 7);
+
+    cout << "\n";
+    cout << "Original: ";
+    cout << list3 << endl;
+
+    cout << "\n";
+    list3.sort();
+    cout << "Sorted: ";
+    cout << list3 << endl;
+
+    cout << "\n";
+    list3.reverse();
+    cout << "Reversed: ";
+    cout << list3 << endl;
 
 }
